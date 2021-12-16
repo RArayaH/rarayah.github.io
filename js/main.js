72c0f06484e87ec3483885939a29ac8484789a91
RArayaH <81941016+RArayaH@users.noreply.github.com>
2021-12-16 16:31:33 -0300

    'main'

--- js/main.js
+++ js/main.js
@@ -4,9 +4,6 @@ const button = document.querySelector('.download')
 if (ua.device !== null) {
     if (ua.device.type === 'Mobile'){
         button.textContent = "Descargue la app";
-        button.href="assets/dpl/app-release.apk";  
-    }else if(ua.device.type === 'Desktop'){
-        button.textContent = "Obtener QR para descargar app";        
-        button.href="assets/img/qr.png"; 
+        button.href="assets/dpl/app-release.apk";
     } 
 }    
