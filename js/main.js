6d0e95931366b14dab3622deab492c93dabe5b0b
RArayaH <81941016+RArayaH@users.noreply.github.com>
2021-12-16 16:29:00 -0300

    'main'

--- js/main.js
+++ js/main.js
@@ -2,11 +2,11 @@ var ua = detect.parse(navigator.userAgent)
 const button = document.querySelector('.download')
 
 if (ua.device !== null) {
-    if (ua.device.family === 'android' || ua.device.family === 'iPhone'){
-        button.textContent = "Descargue la app"
+    if (ua.device.type === 'Mobile'){
+        button.textContent = "Descargue la app";
         button.href="assets/dpl/app-release.apk";  
-    }
-}
-
-
-    
+    }else if(ua.device.type === 'Desktop'){
+        button.textContent = "Obtener QR para descargar app";        
+        button.href="assets/img/qr.png"; 
+    } 
+}    
